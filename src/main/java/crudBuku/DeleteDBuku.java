d6e080db5d644a1158e1ee7e34b2cb68ba5ba84c
HRaisal15 <hafidz.raisal469@student.uns.ac.id>
2023-11-13 08:42:25 +0700

    deleteDbBuku
    update isi DeleteBuku

--- src/main/java/crudBuku/DeleteDBuku.java
+++ src/main/java/crudBuku/DeleteDBuku.java
@@ -3,11 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package crudBuku;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 /**
  *
  * @author hafid punyaa
  */
 public class DeleteDBuku {
+    String url = "jdbc:mysql://localhost:3306/database_perpustakaan";
+    String username = "root";
+    String password = "";
     
+     public void deleteData(String kode_buku){
+        try{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection koneksi = DriverManager.getConnection(url,username,  password);
+            String query = String.format("delete from data_buku where kode_buku = \"%s\";", kode_buku);
+            Statement st = koneksi.createStatement();
+            st.executeUpdate(query);
+            st.close();
+            System.out.println("Koneksi ditutup...");
+
+         }catch (ClassNotFoundException | SQLException ex){
+           System.out.println("Terdapat Error : "+ex.getMessage());  
+       }
+    }
+}    
 }
